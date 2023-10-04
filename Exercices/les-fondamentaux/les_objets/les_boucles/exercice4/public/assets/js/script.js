9571faad9951d9ffcfafe025854e59e27289be4d
Dugenest <143597412+Dugenest@users.noreply.github.com>
2023-10-04 22:37:05 +0200

    Delete Exercices/les-fondamentaux/les_objets/les_boucles directory

--- Exercices/les-fondamentaux/les_objets/les_boucles/exercice4/public/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// Création d'un tableau
-const days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-
-// Création et affichage d'une boucle
-days.forEach(function (day, index){
-    console.log(`${index + 1}: ${day}`);}
-)
