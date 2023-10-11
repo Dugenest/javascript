4b8a0731db417bc3d441eb2067b4b3cd5c463fc8
Dugenest <sebastien.dugenest@gmail.com>
2023-10-11 19:51:07 +0200

    ajout exo

--- tmmdb/public/assets/js/script.js
+++ tmmdb/public/assets/js/script.js
@@ -1,15 +1,32 @@
-// const myImage1 = document.getElementById("399566");
-
+// Récupération données fichier json
 fetch('movies.json')
     .then((response)=>{
         return response.json()
     })
     .then((datas)=>{
-        console.log(datas)
-        count = 0;
-        datas.forEach(data => {
-            console.log(data [count]);
-            count ++;
+        // console.log(datas)
+// Création de la boucle map pour l'objet
+        datas.results.map(data => {
+// Création des variables et récupération de chaque paramètre des objets (sauf les Id)
+            let original_title = data.original_title;
+            let overview = data.overview;
+            let poster_path = data.poster_path;
+            let vote_average = data.vote_average;
+// Création de la structure des cards
+            let card = `<div class="col-md-3 card">
+            <img src="${poster_path}" class="card-img-top card-image" alt="...">
+            <div class="card-body">
+              <h5 id="card-title" class="card-title">${original_title}</h5>
+              <details>
+                <p class="card-text">${overview}</p>
+              </details>
+              <p id="card_note">Note : ${vote_average}</p>
+            </div>
+          </div>`
+// Récupération de l'Id "lesCards"
+            const divCard = document.getElementById('lesCards')
+// Multiplication de la structure et insertion des données
+            divCard.innerHTML += card
         });
-    })
+    });
     
